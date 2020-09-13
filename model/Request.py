fc2062bc808cb8e33985eca63495492c78eba99a
brenobeirigo <brenobeirigo@gmail.com>
2020-09-13 18:32:55 +0200

    Update Request.py

--- model/Request.py
+++ model/Request.py
@@ -1,27 +1,25 @@
-from model.Coordinate import Coordinate
-from model.Node import *
 from collections import defaultdict
-import time
+
 import pandas as pd
+
+from model.Node import *
 
 
 class Request(object):
-
     count = 0
 
     parcel_lockers = set(['XS', 'S', 'M', 'L', 'XL'])
     seats = set(['A', 'C', 'B', 'I'])
-    
+
     # Store the overall pairs
     node_pairs_dict = {}
     od_set = set()
 
     service_quality = defaultdict(list)
 
-
     @classmethod
     def reset_elements(cls):
-        
+
         cls.count = 0
         # Store the overall pairs
         cls.node_pairs_dict = {}
@@ -30,19 +28,19 @@ class Request(object):
         cls.service_quality = defaultdict(list)
 
     def __init__(
-        self,
-        revealing_datetime,
-        max_pickup_delay,
-        max_total_delay,
-        pickup_id,
-        dropoff_id,
-        demand,
-        pickup_latitude=None,
-        pickup_longitude=None,
-        dropoff_latitude=None,
-        dropoff_longitude=None,
-        service_class=None,
-        service_duration = 0):
+            self,
+            revealing_datetime,
+            max_pickup_delay,
+            max_total_delay,
+            pickup_id,
+            dropoff_id,
+            demand,
+            pickup_latitude=None,
+            pickup_longitude=None,
+            dropoff_latitude=None,
+            dropoff_longitude=None,
+            service_class=None,
+            service_duration=0):
         """[summary]
         
         Arguments:
@@ -62,8 +60,6 @@ class Request(object):
             service_class {[type]} -- [description] (default: {None})
         """
 
-
-
         # Invert the demand for destination nodes
         # e.g.: from(p1:1, p2:2) and to(p1:-1, p2:-2)
         if isinstance(demand, dict):
@@ -73,27 +69,26 @@ class Request(object):
             }
         else:
             destination_demand = -demand
-            
+
         # Create origin node
         self.origin = Node.factory_node(
             Node.TYPE_ORIGIN,
             pickup_longitude,
             pickup_latitude,
-            demand = demand,
+            demand=demand,
             parent=self,
-            network_node_id = pickup_id,
-            service_duration = service_duration)
-
+            network_node_id=pickup_id,
+            service_duration=service_duration)
 
         self.destination = Node.factory_node(
             Node.TYPE_DESTINATION,
             dropoff_longitude,
             dropoff_latitude,
-            demand = destination_demand,
+            demand=destination_demand,
             parent=self,
-            network_node_id = dropoff_id,
-            service_duration = service_duration)
-        
+            network_node_id=dropoff_id,
+            service_duration=service_duration)
+
         # Which vehicle picked up the request?
         self.serviced_by = None
 
@@ -103,7 +98,7 @@ class Request(object):
         Request.od_set.add((self.origin, self.destination))
         self.id = Request.count
 
-        Request.count+=1
+        Request.count += 1
         # use revealing_datetime.timestamp() to get an integer
         self.revealing_datetime = revealing_datetime
         self.demand = demand
@@ -123,7 +118,7 @@ class Request(object):
         self.pk_delay = -1
         self.ride_delay = -1
         self.tier = -1
-    
+
     @property
     def max_in_vehicle_delay(self):
         """Maximum time a user can spent inside the vehicle due to
@@ -160,61 +155,60 @@ class Request(object):
 
     def __str__(self):
         return '{sclass}[{id:03}]'.format(
-            sclass = (self.service_class if self.service_class else "R"),
-            id = self.id
-        ) 
+            sclass=(self.service_class if self.service_class else "R"),
+            id=self.id
+        )
 
     def get_info(self, min_dist=None):
-        
+
         return ("{id}({demand}) <{revealing_datetime}> ##"
-        "{origin_id} -> {destination_id} "
-        " - SL: ({pickup_delay:>3}/{pk_delay}, {total_delay:>3}/{dl_delay})"
-        " - Pk.: {arrival}"
-        " - Dp.: {departure}"
-        " - Serviced by: {serviced_by}").format(
-            id = self.__str__(),
-            revealing_datetime = self.revealing_datetime.strftime('%H:%M:%S'),
-            origin_id = self.origin,
-            demand = self. demand,
-            destination_id = self.destination,
-            dl_delay = (self.max_total_delay if self.max_total_delay != None else "-"),
-            pk_delay = (self.max_pickup_delay if self.max_pickup_delay != None else "-"),
-            arrival = (
+                "{origin_id} -> {destination_id} "
+                " - SL: ({pickup_delay:>3}/{pk_delay}, {total_delay:>3}/{dl_delay})"
+                " - Pk.: {arrival}"
+                " - Dp.: {departure}"
+                " - Serviced by: {serviced_by}").format(
+            id=self.__str__(),
+            revealing_datetime=self.revealing_datetime.strftime('%H:%M:%S'),
+            origin_id=self.origin,
+            demand=self.demand,
+            destination_id=self.destination,
+            dl_delay=(self.max_total_delay if self.max_total_delay != None else "-"),
+            pk_delay=(self.max_pickup_delay if self.max_pickup_delay != None else "-"),
+            arrival=(
                 self.origin.arrival.strftime('%H:%M:%S')
                 if self.origin.arrival else '--:--:--'
             ),
-            departure = (
+            departure=(
                 self.destination.arrival.strftime('%H:%M:%S')
                 if self.destination.arrival else '--:--:--'
             ),
-            serviced_by = (
+            serviced_by=(
                 self.serviced_by if self.serviced_by
                 else '-'
             ),
-            total_delay = (
+            total_delay=(
                 int(
                     (self.destination.arrival
-                    - (self.revealing_datetime + timedelta(seconds=min_dist))
-                    ).total_seconds()
+                     - (self.revealing_datetime + timedelta(seconds=min_dist))
+                     ).total_seconds()
                 )
                 if self.serviced_by and min_dist else '-'
             ),
-            pickup_delay = (
+            pickup_delay=(
                 int(
                     (self.origin.arrival - self.revealing_datetime)
-                    .total_seconds()
+                        .total_seconds()
                 )
                 if self.serviced_by else '-'
             )
         )
-    
+
     @staticmethod
     def df_to_request_list(df, service_quality_dict):
-        
+
         requests = []
 
         for row in df.itertuples():
-
             r = Request(
                 row.Index,
                 service_quality_dict[row.service_class]["pk_delay"],
@@ -229,14 +223,14 @@ class Request(object):
                 service_class=row.service_class,
                 service_duration=0,
             )
-            
+
             requests.append(r)
 
         return requests
 
     @staticmethod
     def get_request_list(file_path, service_quality_dict):
-        
+
         df = pd.read_csv(
             file_path,
             parse_dates=True,
@@ -246,7 +240,6 @@ class Request(object):
         requests = []
 
         for row in df.itertuples():
-
             r = Request(
                 row.Index,
                 service_quality_dict[row.service_class]["pk_delay"],
